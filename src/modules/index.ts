87b35cab494d4808bccec561e884413726d1d2f8
MrBBot <me@mrbbot.dev>
2021-05-20 10:14:51 +0100

    Export modules

---
+++ src/modules/index.ts
@@ -0,0 +1,28 @@
+export { Cache } from "./cache";
+export { FetchEvent, ScheduledEvent, ResponseWaitUntil } from "./events";
+export {
+  URL,
+  URLSearchParams,
+  TextDecoder,
+  TextEncoder,
+  fetch,
+  Headers,
+  Request,
+  Response,
+  ByteLengthQueuingStrategy,
+  CountQueuingStrategy,
+  ReadableByteStreamController,
+  ReadableStream,
+  ReadableStreamBYOBReader,
+  ReadableStreamBYOBRequest,
+  ReadableStreamDefaultController,
+  ReadableStreamDefaultReader,
+  TransformStream,
+  TransformStreamDefaultController,
+  WritableStream,
+  WritableStreamDefaultController,
+  WritableStreamDefaultWriter,
+  atob,
+  btoa,
+  crypto,
+} from "./standards";
---
+++ src/modules/modules.ts
@@ -0,0 +1,5 @@
+export { CacheModule } from "./cache";
+export { EventsModule } from "./events";
+export { KVModule } from "./kv";
+export { SitesModule } from "./sites";
+export { StandardsModule } from "./standards";
