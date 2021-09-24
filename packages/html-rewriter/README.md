231e071cd6acc6379728668e47acf9dcee47fa10
Brendan Coll <bcoll@cloudflare.com>
2021-09-24 15:10:12 +0100

    Add `@miniflare/html-rewriter` package

---
+++ packages/html-rewriter/README.md
@@ -0,0 +1,1 @@
+# `@miniflare/html-rewriter`
--- packages/html-rewriter/src/rewriter.ts
+++ packages/html-rewriter/src/rewriter.ts
@@ -1,23 +1,13 @@
 import { ReadableStream } from "stream/web";
 import { TextEncoder } from "util";
-import {
-  HTMLRewriter as BaseHTMLRewriter,
-  Comment,
-  ContentTypeOptions,
-  Doctype,
-  DocumentEnd,
-  DocumentHandlers,
-  Element,
-  ElementHandlers,
-  TextChunk,
-} from "html-rewriter-wasm";
-import { Context, Module } from "./module";
-import { Response } from "./standards";
+import { Response } from "@miniflare/core";
+import type { DocumentHandlers, ElementHandlers } from "html-rewriter-wasm";
+import { Response as BaseResponse } from "undici";
 
 // Based on https://developer.mozilla.org/en-US/docs/Web/API/TransformStream#anything-to-uint8array_stream
 const encoder = new TextEncoder();
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export function transformToArray(chunk: any): Uint8Array {
+function transformToArray(chunk: any): Uint8Array {
   if (chunk instanceof Uint8Array) {
     return chunk;
   } else if (ArrayBuffer.isView(chunk)) {
@@ -32,41 +22,49 @@ export function transformToArray(chunk: any): Uint8Array {
   } else if (typeof chunk === "number") {
     return new Uint8Array([chunk]);
   } else if (chunk === null || chunk === undefined) {
-    throw new TypeError("chunk must be defined");
+    throw new TypeError("Chunk must be defined");
   } else {
     return encoder.encode(String(chunk));
   }
 }
 
+const kElementHandlers = Symbol("kElementHandlers");
+const kDocumentHandlers = Symbol("kDocumentHandlers");
+
+type SelectorElementHandlers = [selector: string, handlers: ElementHandlers];
+
 export class HTMLRewriter {
-  #elementHandlers: [selector: string, handlers: ElementHandlers][] = [];
-  #documentHandlers: DocumentHandlers[] = [];
+  private readonly [kElementHandlers]: SelectorElementHandlers[] = [];
+  private readonly [kDocumentHandlers]: DocumentHandlers[] = [];
 
   on(selector: string, handlers: ElementHandlers): this {
-    this.#elementHandlers.push([selector, handlers]);
+    this[kElementHandlers].push([selector, handlers]);
     return this;
   }
 
   onDocument(handlers: DocumentHandlers): this {
-    this.#documentHandlers.push(handlers);
+    this[kDocumentHandlers].push(handlers);
     return this;
   }
 
-  transform(response: Response): Response {
+  transform(response: BaseResponse): Response {
     const transformedStream = new ReadableStream({
       type: "bytes",
       start: async (controller) => {
         // Create a rewriter instance for this transformation that writes its
         // output to the transformed response's stream
+        const { HTMLRewriter: BaseHTMLRewriter } = await import(
+          "html-rewriter-wasm"
+        );
         const rewriter = new BaseHTMLRewriter((output: Uint8Array) => {
           // enqueue will throw on empty chunks
           if (output.length !== 0) controller.enqueue(output);
         });
         // Add all registered handlers
-        for (const [selector, handlers] of this.#elementHandlers) {
+        for (const [selector, handlers] of this[kElementHandlers]) {
           rewriter.on(selector, handlers);
         }
-        for (const handlers of this.#documentHandlers) {
+        for (const handlers of this[kDocumentHandlers]) {
           rewriter.onDocument(handlers);
         }
 
@@ -88,22 +86,9 @@ export class HTMLRewriter {
       },
     });
 
-    // Return a response with the transformed body, copying over headers, etc
+    // Return a response with the transformed body, copying over headers, etc,
+    // returning a @miniflare/core Response so we don't need to convert
+    // BaseResponse to one when dispatching fetch events
     return new Response(transformedStream, response);
   }
 }
-
-export class HTMLRewriterModule extends Module {
-  buildSandbox(): Context {
-    return { HTMLRewriter };
-  }
-}
-
-export {
-  Element,
-  Comment,
-  TextChunk,
-  Doctype,
-  DocumentEnd,
-  ContentTypeOptions,
-};
