409fc68d1242fd1ec81375490f448cdc1e35089d
MrBBot <me@mrbbot.dev>
2021-11-17 22:41:00 +0000

    Add more helpful error on Durable Object `deserialize` failure

---
+++ packages/durable-objects/src/error.ts
@@ -0,0 +1,9 @@
+import { MiniflareError } from "@miniflare/shared";
+
+export type DurableObjectErrorCode =
+  | "ERR_SCRIPT_NOT_FOUND" // Missing mounted script for object
+  | "ERR_CLASS_NOT_FOUND" // Missing constructor for object
+  | "ERR_RESPONSE_TYPE" // Fetch handler returned non-Response object;
+  | "ERR_DESERIALIZATION"; // Unable to deserialize stored value
+
+export class DurableObjectError extends MiniflareError<DurableObjectErrorCode> {}
