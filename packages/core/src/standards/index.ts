ae057b17345e897a9049e3089cee42566a0b9659
MrBBot <me@mrbbot.dev>
2021-12-15 15:56:06 +0000

    Re-export remaining `fetch` types from `undici`, closes #124

--- packages/core/src/standards/index.ts
+++ packages/core/src/standards/index.ts
@@ -18,8 +18,25 @@ export {
   _buildUnknownProtocolWarning,
   createCompatFetch,
   logResponse,
+  // Re-exported from undici
+  Headers,
 } from "./http";
-export type { RequestInfo, RequestInit, ResponseInit, HRTime } from "./http";
+export type {
+  RequestInfo,
+  RequestInit,
+  ResponseInit,
+  HRTime,
+  // Re-exported from undici
+  BodyInit,
+  HeadersInit,
+  RequestCache,
+  RequestCredentials,
+  RequestDestination,
+  RequestMode,
+  RequestRedirect,
+  ResponseType,
+  ResponseRedirectStatus,
+} from "./http";
 export { FixedLengthStream } from "./streams";
 export type { ArrayBufferViewConstructor } from "./streams";
 export * from "./timers";
