053a570a8b227421d8bc61b71d436894d8c3ec43
MrBBot <me@mrbbot.dev>
2021-12-18 19:17:12 +0000

    Move `CHANGELOG.md` to `docs`
    ...so it's inside the Cloudflare Pages root directory

--- docs/changelog.mjs
+++ docs/changelog.mjs
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export const changelogPath = path.resolve(__dirname, "..", "CHANGELOG.md");
+export const changelogPath = path.resolve(__dirname, "CHANGELOG.md");
 
 const docsChangelogPath = path.resolve(
   __dirname,
