780326ab1cc0dcc6e99c1ea90654aeebd9f10046
MrBBot <me@mrbbot.dev>
2022-01-02 17:19:38 +0000

    Allow `Log` prefix to be customised

--- packages/shared/test/log.spec.ts
+++ packages/shared/test/log.spec.ts
@@ -40,3 +40,17 @@ test.serial("Log: only logs messages less than or equal level", (t) => {
   t.regex(logs[0], /^\[mf:err] Error: Test error/);
   t.is(logs[1], "[mf:wrn] Warning");
 });
+test.serial("Log: uses custom prefix and suffix", (t) => {
+  const logs = interceptConsoleLogs(t);
+  let log = new Log(LogLevel.VERBOSE, { prefix: "pre" });
+  log.warn("Warning");
+  log = new Log(LogLevel.VERBOSE, { prefix: "", suffix: "suf" });
+  log.info("Info");
+  log = new Log(LogLevel.VERBOSE, { prefix: "pre", suffix: "suf" });
+  log.debug("Debug");
+  t.deepEqual(logs, [
+    "[pre:wrn] Warning",
+    "[inf:suf] Info",
+    "[pre:dbg:suf] Debug",
+  ]);
+});
