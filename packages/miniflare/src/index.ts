4a3f509f5e85364c980c1705e562e84f5249b42f
MrBBot <me@mrbbot.dev>
2022-05-27 21:45:28 +0100

    Export `Request`, `Response` & related from `miniflare`, closes #258

--- packages/miniflare/src/index.ts
+++ packages/miniflare/src/index.ts
@@ -2,3 +2,5 @@ export * from "./api";
 export * from "./storage";
 export * from "./updater";
 export { Log, LogLevel } from "@miniflare/shared";
+export { Request, Response } from "@miniflare/core";
+export type { RequestInfo, RequestInit } from "@miniflare/core";
