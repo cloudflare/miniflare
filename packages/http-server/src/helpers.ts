dab03dcb78864952f3987394ec5ec432e72744d3
MrBBot <me@mrbbot.dev>
2022-06-11 14:18:30 +0100

    Fix accessible hosts list in Node.js 18
    The `family` property returned by `os.networkInterfaces()` is a
    `number` as of Node.js 18.0.0:
    https://nodejs.org/api/os.html#osnetworkinterfaces
    
    This meant no IPv4 addresses were being found, and subsequently none
    were logged to the console.

--- packages/http-server/src/helpers.ts
+++ packages/http-server/src/helpers.ts
@@ -2,10 +2,11 @@ import { networkInterfaces } from "os";
 
 export function getAccessibleHosts(ipv4 = false): string[] {
   const hosts: string[] = [];
-  Object.values(networkInterfaces()).forEach((net) =>
+  Object.values(networkInterfaces()).forEach((net) => {
     net?.forEach(({ family, address }) => {
-      if (!ipv4 || family === "IPv4") hosts.push(address);
-    })
-  );
+      // @ts-expect-error the `family` property is numeric as of Node.js 18.0.0
+      if (!ipv4 || family === "IPv4" || family === 4) hosts.push(address);
+    });
+  });
   return hosts;
 }
