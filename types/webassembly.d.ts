adb8171ee37f8b258ee1c37f941d71cdb333fea1
Yusef Napora <yusef@napora.org>
2022-08-19 11:10:40 -0400

    fix NotSupportedError when using NODE-ED25519 (#311)
    * fix NotSupportedError when using NODE-ED25519
    
    * Fixes for `NODE-ED25519` support and additional tests
    
    - Require `namedCurve` to be `NODE-ED25519`
    - Gate `Ed25519` usage behind feature detect
    - Use `Ed25519` algorithm object instead of string
    - Override `sign`, `verify` methods too
    - Do not require `importKey` to be within `RequestContext`
    - Automatically add `public: true` when importing `NODE-ED25519` keys
    - Upgrade `@types/node` for improved `webcrypto` types
    - Add tests for other algorithms using modified methods
    
    Co-authored-by: bcoll <bcoll@cloudflare.com>

--- types/webassembly.d.ts
+++ types/webassembly.d.ts
@@ -14,6 +14,8 @@
 // See the Apache Version 2.0 License for specific language governing permissions
 // and limitations under the License.
 
+type BufferSource = ArrayBufferView | ArrayBuffer;
+
 declare namespace WebAssembly {
   class Global {
     constructor(descriptor: GlobalDescriptor, v?: any);
