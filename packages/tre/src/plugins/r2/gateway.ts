5b8f84eb7545ad0354294d8cb47bbc8804d1690b
Somhairle MacLeòid <samuel@macleod.space>
2022-09-06 13:36:27 +0100

    R2 API (#355)
    *  R2 API

--- packages/tre/src/plugins/r2/gateway.ts
+++ packages/tre/src/plugins/r2/gateway.ts
@@ -1,24 +1,218 @@
-import { Clock, Storage } from "@miniflare/shared";
+import { RangeStoredValueMeta, Storage } from "@miniflare/shared";
+import { InvalidRange, NoSuchKey } from "./errors";
+import {
+  R2HTTPMetadata,
+  R2Object,
+  R2ObjectBody,
+  R2ObjectMetadata,
+  createVersion,
+} from "./r2Object";
+import { Validator } from "./validator";
+
+// For more information, refer to https://datatracker.ietf.org/doc/html/rfc7232
+export interface R2Conditional {
+  // Performs the operation if the object’s etag matches the given string.
+  etagMatches?: string;
+  // Performs the operation if the object’s etag does not match the given string.
+  etagDoesNotMatch?: string;
+  // Performs the operation if the object was uploaded before the given date.
+  uploadedBefore?: number;
+  // Performs the operation if the object was uploaded after the given date.
+  uploadedAfter?: number;
+}
+
+export interface R2Range {
+  offset?: number;
+  length?: number;
+  suffix?: number;
+}
+
+export interface R2GetOptions {
+  // Specifies that the object should only be returned given satisfaction of
+  // certain conditions in the R2Conditional. Refer to R2Conditional above.
+  onlyIf?: R2Conditional;
+  // Specifies that only a specific length (from an optional offset) or suffix
+  // of bytes from the object should be returned. Refer to
+  // https://developers.cloudflare.com/r2/runtime-apis/#ranged-reads.
+  range?: R2Range;
+}
+
+export interface R2PutOptions {
+  // Various HTTP headers associated with the object. Refer to
+  // https://developers.cloudflare.com/r2/runtime-apis/#http-metadata.
+  httpMetadata: R2HTTPMetadata;
+  // A map of custom, user-defined metadata that will be stored with the object.
+  customMetadata: Record<string, string>;
+  // A md5 hash to use to check the recieved object’s integrity.
+  md5?: string;
+}
+
+export type R2ListOptionsInclude = ("httpMetadata" | "customMetadata")[];
+
+export interface R2ListOptions {
+  // The number of results to return. Defaults to 1000, with a maximum of 1000.
+  limit?: number;
+  // The prefix to match keys against. Keys will only be returned if they start with given prefix.
+  prefix?: string;
+  // An opaque token that indicates where to continue listing objects from.
+  // A cursor can be retrieved from a previous list operation.
+  cursor?: string;
+  // The character to use when grouping keys.
+  delimiter?: string;
+  // Can include httpFields and/or customFields. If included, items returned by
+  // the list will include the specified metadata. Note that there is a limit on the
+  // total amount of data that a single list operation can return.
+  // If you request data, you may recieve fewer than limit results in your response
+  // to accomodate metadata.
+  // Use the truncated property to determine if the list request has more data to be returned.
+  include?: R2ListOptionsInclude;
+}
+
+export interface R2Objects {
+  // An array of objects matching the list request.
+  objects: R2Object[];
+  // If true, indicates there are more results to be retrieved for the current list request.
+  truncated: boolean;
+  // A token that can be passed to future list calls to resume listing from that point.
+  // Only present if truncated is true.
+  cursor?: string;
+  // If a delimiter has been specified, contains all prefixes between the specified
+  // prefix and the next occurence of the delimiter.
+  // For example, if no prefix is provided and the delimiter is ‘/’, foo/bar/baz
+  // would return foo as a delimited prefix. If foo/ was passed as a prefix
+  // with the same structure and delimiter, foo/bar would be returned as a delimited prefix.
+  delimitedPrefixes: string[];
+}
+
+const MAX_LIST_KEYS = 1_000;
+// https://developers.cloudflare.com/r2/platform/limits/ (5GB - 5MB)
+
+const validate = new Validator();
 
 export class R2Gateway {
-  constructor(
-    private readonly storage: Storage,
-    private readonly clock: Clock
-  ) {}
+  constructor(private readonly storage: Storage) {}
 
-  async get(key: string) {
-    throw new Error("Not yet implemented!");
+  async head(key: string): Promise<R2Object> {
+    validate.key(key);
+
+    // Get value, returning null if not found
+    const stored = await this.storage.head<R2ObjectMetadata>(key);
+
+    if (stored?.metadata === undefined) throw new NoSuchKey();
+    const { metadata } = stored;
+
+    return new R2Object(metadata);
   }
 
-  async put(key: string, value: Uint8Array) {
-    throw new Error("Not yet implemented!");
+  async get(
+    key: string,
+    options: R2GetOptions = {}
+  ): Promise<R2ObjectBody | R2Object> {
+    const { range = {}, onlyIf } = options;
+    validate
+      .key(key)
+      .getOptions(options)
+      .condition(await this.head(key), onlyIf);
+
+    let stored: RangeStoredValueMeta<R2ObjectMetadata> | undefined;
+
+    // get data dependent upon whether suffix or range exists
+    try {
+      stored = await this.storage.getRange<R2ObjectMetadata>(key, range);
+    } catch {
+      throw new InvalidRange();
+    }
+    if (stored?.metadata === undefined) throw new NoSuchKey();
+    const { value, metadata } = stored;
+    // add range should it exist
+    if ("range" in stored && stored.range !== undefined) {
+      metadata.range = stored.range;
+    }
+
+    return new R2ObjectBody(metadata, value);
+  }
+
+  async put(
+    key: string,
+    value: Uint8Array,
+    options: R2PutOptions
+  ): Promise<R2Object> {
+    const { customMetadata, md5, httpMetadata } = options;
+
+    const hash = validate
+      .key(key)
+      .putOptions(options)
+      .size(value)
+      .md5(value, md5);
+
+    // build metadata
+    const metadata: R2ObjectMetadata = {
+      key,
+      size: value.byteLength,
+      etag: hash,
+      version: createVersion(),
+      httpEtag: `"${hash}"`,
+      uploaded: Date.now(),
+      httpMetadata,
+      customMetadata,
+    };
+
+    // Store value with expiration and metadata
+    await this.storage.put<R2ObjectMetadata>(key, {
+      value,
+      metadata,
+    });
+
+    return new R2Object(metadata);
   }
 
   async delete(key: string) {
-    throw new Error("Not yet implemented!");
+    validate.key(key);
+    await this.storage.delete(key);
   }
 
-  async list() {
-    throw new Error("Not yet implemented!");
+  async list(listOptions: R2ListOptions = {}): Promise<R2Objects> {
+    const delimitedPrefixes = new Set<string>();
+
+    validate.listOptions(listOptions);
+
+    const { prefix = "", include = [], cursor = "" } = listOptions;
+    let { delimiter, limit = MAX_LIST_KEYS } = listOptions;
+    if (delimiter === "") delimiter = undefined;
+
+    // if include contains inputs, we reduce the limit to max 100
+    if (include.length > 0) limit = Math.min(limit, 100);
+
+    const res = await this.storage.list<R2ObjectMetadata>({
+      prefix,
+      limit,
+      cursor,
+      delimiter,
+    });
+    // add delimited prefixes should they exist
+    for (const dP of res.delimitedPrefixes ?? []) delimitedPrefixes.add(dP);
+
+    const objects = res.keys
+      // grab metadata
+      .map((k) => k.metadata)
+      // filter out objects that exist within the delimiter
+      .filter(
+        (metadata): metadata is R2ObjectMetadata => metadata !== undefined
+      )
+      // filter "httpFields" and/or "customFields" if found in "include"
+      .map((metadata) => {
+        if (!include.includes("httpMetadata")) metadata.httpMetadata = {};
+        if (!include.includes("customMetadata")) metadata.customMetadata = {};
+
+        return new R2Object(metadata);
+      });
+
+    const cursorLength = res.cursor.length > 0;
+    return {
+      objects,
+      truncated: cursorLength,
+      cursor: cursorLength ? res.cursor : undefined,
+      delimitedPrefixes: [...delimitedPrefixes],
+    };
   }
 }
---
+++ packages/tre/src/plugins/r2/r2Object.ts
@@ -0,0 +1,141 @@
+import crypto from "crypto";
+import { TextEncoder } from "util";
+import { R2Objects, R2Range } from "./gateway";
+
+const encoder = new TextEncoder();
+export interface R2HTTPMetadata {
+  contentType?: string;
+  contentLanguage?: string;
+  contentDisposition?: string;
+  contentEncoding?: string;
+  cacheControl?: string;
+  cacheExpiry?: Date;
+}
+
+export interface R2ObjectMetadata {
+  // The object’s key.
+  key: string;
+  // Random unique string associated with a specific upload of a key.
+  version: string;
+  // Size of the object in bytes.
+  size: number;
+  // The etag associated with the object upload.
+  etag: string;
+  // The object’s etag, in quotes so as to be returned as a header.
+  httpEtag: string;
+  // The time the object was uploaded.
+  uploaded: number;
+  // Various HTTP headers associated with the object. Refer to HTTP Metadata.
+  httpMetadata: R2HTTPMetadata;
+  // A map of custom, user-defined metadata associated with the object.
+  customMetadata: Record<string, string>;
+  // If a GET request was made with a range option, this will be added
+  range?: R2Range;
+}
+
+// R2ObjectMetadata in the format the Workers Runtime expects to be returned
+export interface RawR2ObjectMetadata
+  extends Omit<R2ObjectMetadata, "key" | "httpMetadata" | "customMetadata"> {
+  // The object’s name.
+  name: string;
+  // Various HTTP headers associated with the object. Refer to HTTP Metadata.
+  httpFields: R2HTTPMetadata;
+  // A map of custom, user-defined metadata associated with the object.
+  customFields: { k: string; v: string }[];
+}
+
+interface EncodedMetadata {
+  metadataSize: number;
+  value: Uint8Array;
+}
+
+export function createVersion(): string {
+  return crypto.randomBytes(24).toString("base64");
+}
+
+/**
+ * R2Object is created when you PUT an object into an R2 bucket.
+ * R2Object represents the metadata of an object based on the information
+ * provided by the uploader. Every object that you PUT into an R2 bucket
+ * will have an R2Object created.
+ */
+export class R2Object implements R2ObjectMetadata {
+  // The object’s key.
+  key: string;
+  // Random unique string associated with a specific upload of a key.
+  version: string;
+  // Size of the object in bytes.
+  size: number;
+  // The etag associated with the object upload.
+  etag: string;
+  // The object’s etag, in quotes so as to be returned as a header.
+  httpEtag: string;
+  // The time the object was uploaded.
+  uploaded: number;
+  // Various HTTP headers associated with the object. Refer to
+  // https://developers.cloudflare.com/r2/runtime-apis/#http-metadata.
+  httpMetadata: R2HTTPMetadata;
+  // A map of custom, user-defined metadata associated with the object.
+  customMetadata: Record<string, string>;
+  // If a GET request was made with a range option, this will be added
+  range?: R2Range;
+  constructor(metadata: R2ObjectMetadata) {
+    this.key = metadata.key;
+    this.version = metadata.version;
+    this.size = metadata.size;
+    this.etag = metadata.etag;
+    this.httpEtag = metadata.httpEtag;
+    this.uploaded = metadata.uploaded;
+    this.httpMetadata = metadata.httpMetadata;
+    this.customMetadata = metadata.customMetadata;
+    this.range = metadata.range;
+  }
+
+  // Format for return to the Workers Runtime
+  rawProperties(): RawR2ObjectMetadata {
+    return {
+      ...this,
+      name: this.key,
+      httpFields: this.httpMetadata,
+      customFields: Object.entries(this.customMetadata).map(([k, v]) => ({
+        k,
+        v,
+      })),
+    };
+  }
+
+  encode(): EncodedMetadata {
+    const json = JSON.stringify(this.rawProperties());
+    const bytes = encoder.encode(json);
+    return { metadataSize: bytes.length, value: bytes };
+  }
+
+  static encodeMultiple(objects: R2Objects): EncodedMetadata {
+    const json = JSON.stringify({
+      ...objects,
+      objects: objects.objects.map((o) => o.rawProperties()),
+    });
+    const bytes = encoder.encode(json);
+    return { metadataSize: bytes.length, value: bytes };
+  }
+}
+
+export class R2ObjectBody extends R2Object {
+  body: Uint8Array;
+
+  constructor(metadata: R2ObjectMetadata, body: Uint8Array) {
+    super(metadata);
+    this.body = body;
+  }
+
+  encode(): EncodedMetadata {
+    const { metadataSize, value: metadata } = super.encode();
+    const merged = new Uint8Array(metadataSize + this.body.length);
+    merged.set(metadata);
+    merged.set(this.body, metadataSize);
+    return {
+      metadataSize: metadataSize,
+      value: merged,
+    };
+  }
+}
