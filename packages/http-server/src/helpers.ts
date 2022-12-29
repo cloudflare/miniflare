f6f52f6a85a648b3f041474092f86f06cc027b4f
Frederik Baetens <fbaetens@cloudflare.com>
2022-12-29 15:07:55 +0000

    add comment about the family property no longer being numeric

--- packages/http-server/src/helpers.ts
+++ packages/http-server/src/helpers.ts
@@ -4,6 +4,8 @@ export function getAccessibleHosts(ipv4 = false): string[] {
   const hosts: string[] = [];
   Object.values(networkInterfaces()).forEach((net) => {
     net?.forEach(({ family, address }) => {
+      // The `family` property being numeric was reverted in Node 18.2
+      // https://github.com/nodejs/node/issues/43014
       // @ts-expect-error the `family` property is numeric as of Node.js 18.0.0
       if (!ipv4 || family === "IPv4" || family === 4) hosts.push(address);
     });
