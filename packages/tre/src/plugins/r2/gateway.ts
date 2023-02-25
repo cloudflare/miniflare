3383f592f14a05bc66c3f251709b01ae33679f59
bcoll <bcoll@cloudflare.com>
2023-02-25 13:45:00 +0000

    Support ranged-`R2Bucket#get()` with `Range` header
    Ref: https://community.cloudflare.com/t/2022-8-12-workers-runtime-release-notes/410873

--- packages/tre/src/plugins/r2/gateway.ts
+++ packages/tre/src/plugins/r2/gateway.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { Log } from "../../shared";
 import { RangeStoredValueMeta, Storage } from "../../storage";
+import { _parseRanges } from "../shared";
 import { InvalidRange, NoSuchKey } from "./errors";
 import {
   R2Object,
@@ -59,12 +60,26 @@ export class R2Gateway {
     key: string,
     options: R2GetOptions = {}
   ): Promise<R2ObjectBody | R2Object> {
-    const { range = {}, onlyIf } = options;
-    validate.key(key).condition(await this.head(key), onlyIf);
+    const meta = await this.head(key);
+    validate.key(key).condition(meta, options.onlyIf);
+
+    let range = options.range ?? {};
+    if (options.rangeHeader !== undefined) {
+      const ranges = _parseRanges(options.rangeHeader, meta.size);
+      if (ranges?.length === 1) {
+        // If the header contained a single range, convert it to an R2Range.
+        // Note `start` and `end` are inclusive.
+        const [start, end] = ranges[0];
+        range = { offset: start, length: end - start + 1 };
+      } else {
+        // If the header was invalid, or contained multiple ranges, just return
+        // the full response
+        range = {};
+      }
+    }
 
     let stored: RangeStoredValueMeta<R2ObjectMetadata> | undefined;
 
-    // get data dependent upon whether suffix or range exists
     try {
       stored = await this.storage.getRange<R2ObjectMetadata>(key, range);
     } catch {
