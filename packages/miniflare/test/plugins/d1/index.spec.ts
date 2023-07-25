2b616c7700050c4b71e5bc577fafa24e16ed0a66
bcoll <bcoll@cloudflare.com>
2023-07-25 18:21:11 +0100

    Rewrite D1 tests to use proxy
    D1 tests previously defined a `TestD1Database` stub that made calls
    to custom HTTP endpoints to perform operations. With the new
    `Miniflare#getD1Database()` function, we can simplify the unshimmed
    test using the wrapped binding. Note we still need to use
    `TestD1Database` for the shimmed tests, as the proxy will only give
    us a `Fetcher` binding.

--- packages/miniflare/test/plugins/d1/index.spec.ts
+++ packages/miniflare/test/plugins/d1/index.spec.ts
@@ -1,7 +1,6 @@
-import path from "path";
-import { FIXTURES_PATH } from "./shared";
-import suite from "./suite";
+import { setupTest } from "./test";
 
 // Post-wrangler 3.3, D1 bindings work directly, so use the input file
-// from the fixture, and no prefixc on the binding name
-suite("DB", path.join(FIXTURES_PATH, "d1", "worker.mjs"));
+// from the fixture, and no prefix on the binding name
+setupTest("DB", "worker.mjs", (mf) => mf.getD1Database("DB"));
+require("./suite");
