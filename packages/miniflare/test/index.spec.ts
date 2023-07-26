47ecc144af820a73dbaced0aa12838abf76b9c86
bcoll <bcoll@cloudflare.com>
2023-07-26 11:39:00 +0100

    Use single connection `Client` dispatcher and assert bodies consumed
    When combined with the previous change, this should ensure we only
    have at most two TCP connections open to `workerd` at any given time.
    It also throws an uncaught exception if we forget to consume a
    response body in a test. Unconsumed bodies may cause `undici` to hang
    or throw socket errors.

--- packages/miniflare/test/index.spec.ts
+++ packages/miniflare/test/index.spec.ts
@@ -494,6 +494,7 @@ test("Miniflare: HTTPS fetches using browser CA certificates", async (t) => {
   t.teardown(() => mf.dispose());
   const res = await mf.dispatchFetch("http://localhost");
   t.true(res.ok);
+  await res.arrayBuffer();
 });
 
 test("Miniflare: accepts https requests", async (t) => {
@@ -513,6 +514,7 @@ test("Miniflare: accepts https requests", async (t) => {
 
   const res = await mf.dispatchFetch("https://localhost");
   t.true(res.ok);
+  await res.arrayBuffer();
 
   t.assert(log.logs[0][1].startsWith("Ready on https://"));
 });
