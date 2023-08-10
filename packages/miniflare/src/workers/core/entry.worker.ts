37c58b228bf3222fb5df3de126d6228d7d6a83cc
jjohnson <jjohnson@cloudflare.com>
2023-08-10 11:59:14 -0500

    Trigger scheduled handler on '/cdn-cgi/mf/scheduled'

--- packages/miniflare/src/workers/core/entry.worker.ts
+++ packages/miniflare/src/workers/core/entry.worker.ts
@@ -164,6 +164,24 @@ async function handleQueue(
   return Response.json(queueResponse);
 }
 
+async function handleScheduled(
+  params: URLSearchParams,
+  service: Fetcher
+): Promise<Response> {
+  const time = params.get("time");
+  const scheduledTime = time ? new Date(parseInt(time)) : undefined;
+  const cron = params.get("cron") ?? undefined;
+
+  const result = await service.scheduled({
+    scheduledTime,
+    cron,
+  });
+
+  return new Response(result.outcome, {
+    status: result.outcome === "ok" ? 200 : 500,
+  });
+}
+
 export default <ExportedHandler<Env>>{
   async fetch(request, env, ctx) {
     const startTime = Date.now();
@@ -183,9 +201,12 @@ export default <ExportedHandler<Env>>{
 
     try {
       const customEvent = request.headers.get(CoreHeaders.CUSTOM_EVENT);
-      // TODO(soon): support scheduled events, requires support from workerd
       if (customEvent === "queue") {
         return await handleQueue(request, url, service, startTime);
+      }
+
+      if (url.pathname === "/cdn-cgi/mf/scheduled") {
+        return await handleScheduled(url.searchParams, service);
       }
 
       let response = await service.fetch(request);
