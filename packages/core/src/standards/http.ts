0ecac665383e85a38fa0c19be2f29a79be1e41af
Bruno Nascimento <bruno.nascimento@csghq.com>
2023-10-07 23:02:36 -0300

    fix: unhandled error thrown by busboy
    As per, https://fetch.spec.whatwg.org/#dom-body-formdata, TypeErrors are
    expected to be thrown. Busboy errors were being unhandled.

--- packages/core/src/standards/http.ts
+++ packages/core/src/standards/http.ts
@@ -311,41 +311,51 @@ export class Body<Inner extends BaseRequest | BaseResponse> {
       );
     }
     const formData = new FormData();
-    await new Promise<void>(async (resolve) => {
-      const Busboy: typeof import("busboy") = require("busboy");
-      const busboy = Busboy({
-        headers: headers as http.IncomingHttpHeaders,
-        preservePath: true,
-      });
-      busboy.on("field", (name, value) => {
-        formData.append(name, value);
-      });
-      busboy.on("file", (name, value, info) => {
-        const { filename, encoding, mimeType } = info;
-        const base64 = encoding.toLowerCase() === "base64";
-        const chunks: Buffer[] = [];
-        let totalLength = 0;
-        value.on("data", (chunk: Buffer) => {
-          if (base64) chunk = Buffer.from(chunk.toString(), "base64");
-          chunks.push(chunk);
-          totalLength += chunk.byteLength;
-        });
-        value.on("end", () => {
-          if (this[kFormDataFiles]) {
-            const file = new File(chunks, filename, { type: mimeType });
-            formData.append(name, file);
-          } else {
-            const text = Buffer.concat(chunks, totalLength).toString();
-            formData.append(name, text);
-          }
-        });
-      });
-      busboy.on("finish", resolve);
+    const busboyPromiseResult = await new Promise<void | Error>(
+      async (resolve, reject) => {
+        try {
+          const Busboy: typeof import("busboy") = require("busboy");
+          const busboy = Busboy({
+            headers: headers as http.IncomingHttpHeaders,
+            preservePath: true,
+          });
+          busboy.on("field", (name, value) => {
+            formData.append(name, value);
+          });
+          busboy.on("file", (name, value, info) => {
+            const { filename, encoding, mimeType } = info;
+            const base64 = encoding.toLowerCase() === "base64";
+            const chunks: Buffer[] = [];
+            let totalLength = 0;
+            value.on("data", (chunk: Buffer) => {
+              if (base64) chunk = Buffer.from(chunk.toString(), "base64");
+              chunks.push(chunk);
+              totalLength += chunk.byteLength;
+            });
+            value.on("end", () => {
+              if (this[kFormDataFiles]) {
+                const file = new File(chunks, filename, { type: mimeType });
+                formData.append(name, file);
+              } else {
+                const text = Buffer.concat(chunks, totalLength).toString();
+                formData.append(name, text);
+              }
+            });
+          });
+          busboy.on("finish", resolve);
 
-      const body = this[_kInner].body;
-      if (body !== null) for await (const chunk of body) busboy.write(chunk);
-      busboy.end();
-    });
+          const body = this[_kInner].body;
+          if (body !== null)
+            for await (const chunk of body) busboy.write(chunk);
+          busboy.end();
+        } catch (e) {
+          reject(e);
+        }
+      }
+    );
+    if (busboyPromiseResult instanceof Error) {
+      throw new TypeError(busboyPromiseResult.message);
+    }
     if (this[kInputGated]) await waitForOpenInputGate();
     return formData;
   }
